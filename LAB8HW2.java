f067a072141683c21577fb23dbc84c859a5ee6f2
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-05-06 19:14:33 +0600

    Update LAB8HW2.java
    Added question.

--- LAB8HW2.java
+++ LAB8HW2.java
@@ -1,3 +1,10 @@
+// A. Write a simple method called showDots that takes a number as an argument and then prints that amount of dots inside the method.
+// Note: You can use System.out.print() to avoid the next output being printed on the next line.
+// B. Write a method called show_palindrome that takes a number as an argument and then prints a palindrome inside the method.
+// Note: You can use System.out.print() to avoid the next output being printed on the next line.
+// Write a method called showDiamond that takes an integer number as an argument and then prints a palindromic diamond shape. Moreover, the empty spaces surrounding the diamonds are filled with dots(.) .
+// Note: You must call the methods written in task 2A & 2B, otherwise this task would be considered invalid.
+
 public class LAB8HW2{
   public static void main(String [] args){
     showDiamond(3);
