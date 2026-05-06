504013d9225eff323f32ba2c92e87a958490d3f8
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-05-06 19:12:45 +0600

    Update LAB8HW1.java
    Added question properly.

--- LAB8HW1.java
+++ LAB8HW1.java
@@ -1,8 +1,15 @@
-// Write a method called isPrime which takes an integer in its parameter to
-// check whether a number is prime or not. If the number is prime then the
-// method returns boolean true otherwise it returns boolean false.
+// A. Write a method called isPrime which takes an integer in its parameter to check whether a number is prime or not. 
+// If the number is prime then the method returns boolean true otherwise it returns boolean false.
 
-public class LAB8HW1{
+// B.Write a method called isPerfect which takes an integer in its parameter to check whether a number is perfect or not. 
+// If the number is perfect then the method returns boolean true otherwise it returns boolean false.
+
+// C.Write a method called special_sum that calculates the sum of all numbers that are either prime numbers or perfect up till the integer value given in its parameter. 
+// This integer value must be taken as user input and passed into the method.
+
+// Note: You must call the methods written in task 1A & 1B, otherwise this task will be considered invalid.
+
+  public class LAB8HW1{
   public static void main(String [] args){
     int result = special_sum(8);
     System.out.println(result);
