1ef9d6749a81172cf34497b5c7528a91af724b33
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-05-10 17:46:37 +0600

    Added question.

--- LAB9HW3.java
+++ LAB9HW3.java
@@ -1,3 +1,5 @@
+// Write a recursive method called print_elements(arr, index) that prints elements of an array starting from index to the end.
+
 public class LAB9HW3{
   public static void main(String [] args){
     int[] arr = {5,6,2,1,8,7};
