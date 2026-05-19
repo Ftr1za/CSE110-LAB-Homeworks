39b5498405de78e4ed14a5f46f8da2881f27a37e
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-05-19 22:27:02 +0600

    Added question.

--- LAB7HW5.java
+++ LAB7HW5.java
@@ -1,3 +1,8 @@
+// You are given two arrays of the same length. 
+// The first array contains the marks of the students and the second array contains the name of the students. 
+// You need to sort the marks array in ascending order while maintaining the corresponding names of the students in the names array aligned with their respective marks. 
+// Use the Bubble sort technique to solve this problem. [Your code should work for any given arrays]
+
 public class LAB7HW5{
   public static void main(String [] args){
     int [] marks ={85,90,75,44,99};
